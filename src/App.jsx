879b6652df81ba2079ded4837aa8d164a84bf126
khalid-bensaada <kbensaada953@gmail.com>
2026-07-01 13:15:39 +0100

    first one

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-700">
+      <h1 className="text-6xl font-bold text-white">
+        BioProGreen
+      </h1>
+    </div>
+  );
+}
+
+export default App;
